2a3ae04c2b059b81b6d022d92911befd9f483fc0
Alejo Pugliese <alejopugli@gmail.com>
2022-08-05 23:18:35 -0300

    FIX dockerfile FIX Typo

--- docker/init-import-mongo02.js
+++ docker/init-import-mongo02.js
@@ -1,3 +1,3 @@
-db = db.getSiblingDB('accidents')
+db = db.getSiblingDB('accidents');
 db.accident.updateMany({}, [{$set: {location: { "type": "Point", "coordinates": ["$Start_Lng", "$Start_Lat"]}}}]);
 db.accident.createIndex({location:"2dsphere"});
